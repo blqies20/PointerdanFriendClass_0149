2580b5f5ce5ec5b58e6d67deba751e9010fdc19d
blqies20 <balqis1maharani@gmail.com>
2025-05-06 11:24:23 +0700

    fungsi setNama tidak diketahui punya siapa

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -9,3 +9,16 @@ private:
 public:
     friend void setNama(mahasiswa &a, string);
 };
+
+void setNama(mahasiswa&a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main ()
+{
+    mahasiswa rani;
+    setNama(rani, "Maharani");
+    return 0;
+}
