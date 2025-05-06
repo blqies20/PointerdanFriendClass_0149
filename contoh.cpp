c539ed0234deb1a4483262696f004ed054f83aaf
blqies20 <balqis1maharani@gmail.com>
2025-05-06 10:48:01 +0700

    membuat class hitung lingkaran

--- contoh.cpp
+++ contoh.cpp
@@ -14,4 +14,29 @@ public:
         cout << "Masukkan Lebar : ";
         cin >> lebar;
     }
+
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
+
+    Lingkaran ol;
+};
+
+class Lingkaran
+{
+public:
+    int jarijari;
+
+    void input()
+    {
+        cout << "Masukkan Jari-Jari : ";
+        cin >> jarijari;
+    }
+
+    int luasLingkaran(int r)
+    {
+        return 3.14 * r *r;
+    }
+
 };
